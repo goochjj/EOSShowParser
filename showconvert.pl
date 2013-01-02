6fca797be79e13f5f3b58960e0e659e85153ef42
Joseph Gooch <mrwizard@asok-wireless.home.wiznet>
2013-01-02 00:21:56 -0500

    CGI version

--- showconvert.pl
+++ showconvert.pl
@@ -1,13 +1,35 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -wT
 
 use CGI;
+use CGI::Carp qw ( fatalsToBrowser );
+use File::Basename;
+use FileHandle;
 use strict;
 
+BEGIN {
+  my $src = dirname(__FILE__);
+  require "$src/ParseShowFile.pm";
+}
+
+$CGI::POST_MAX = 1024 * 5000;
+
 my $q = new CGI;
+my $p = new ParseShowFile();
 
-print $q->header();
-
+print $q->header("text/html");
 print $q->start_html();
-print "Hi";
-print $q->end_html();
+print $q->start_head();
+print $q->title("Show Parsing");
+print $p->get_styles();
+print $q->end_head();
+print $q->start_body();
+print '<form method="post" enctype="multipart/form-data"><p>ASCII File: <input type="file" name="showfile" /></p><input type="submit" value="Go" /></form><br/>'."\n";
+if ($q->param("showfile")) {
+  my $fh = $q->upload("showfile");
+  my $data = $p->parse_file($fh);
+  print "<br/><br/>\n";
+  print $p->generate_page($q);
+}
 
+print $q->end_body();
+print $q->end_html();
