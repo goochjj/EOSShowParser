ea0586db62657875ba652fc8391a25e5bba7d300
Joseph Gooch <mrwizard@dok.org>
2013-02-15 22:07:18 -0500

    Deal with show titles, presets, and pallete based parameters

--- ParseShowFile.pm
+++ ParseShowFile.pm
@@ -19,6 +19,7 @@ sub reset {
   $self->{usegroups} = 0;
   $self->{record} = undef;
   $self->{data} = {
+    Title => "Show File",
     ParamType => {},
     ParamNameToType => {},
     Personality => {},
@@ -27,6 +28,7 @@ sub reset {
     BeamPalette => {},
     Patch => {},
     Group => {},
+    Preset => {},
     ChannelToGroups => {}
   };
 }
@@ -35,7 +37,7 @@ sub closerecord {
   my $self = shift;
   if ($self->{record}) {
     if ($self->{record}->{type}) {
-      if ($self->{record}->{type} =~ /^(?:Color|Beam|Focus)Palette$/) {
+      if ($self->{record}->{type} =~ /^(?:(?:Color|Beam|Focus)Palette|Preset)$/) {
         $self->{record}->{parameters} = [ sort { $a <=> $b } keys %{$self->{record}->{parameters}} ];
       }
       if ($self->{record}->{type} eq "Group") {
@@ -56,6 +58,10 @@ sub parse_file {
   $self->reset();
   while(<$fh>) {
     chomp;
+    if (/^\$\$Title\s+(.+)/) {
+      $self->{data}->{Title}= $1;
+      next;
+    }
     if (/^\$ParamType\s+(\d+)\s+(\d+)\s+(\S+)/) {
       $self->{data}->{ParamType}->{$1} = $3;
       $self->{data}->{ParamNameToType}->{$3} = $1;
@@ -64,6 +70,11 @@ sub parse_file {
     if (/^\$((?:Beam|Color|Focus)Palette)\s+(\d+)/) {
       $self->closerecord();
       $self->{record} = { index => $2, type => $1, title=>$2, parameters => {}, channels => {} };
+      next;
+    }
+    if (/^Group\s+(\d+)/) {
+      $self->closerecord();
+      $self->{record} = { index => $1, type => "Preset", title=>$1, parameters => {}, channels => {} };
       next;
     }
     if (/^\$Group\s+(\d+)/) {
@@ -76,9 +87,18 @@ sub parse_file {
       $self->{record} = { index => $1, type => "Personality", model=>"", manufacturer=>"", dcid=>"", params => {} };
       next;
     }
+    if (/^Patch\s+.*/) {
+      $self->closerecord();
+      next;
+    }
     if (/^\$Patch\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)/) {
       $self->closerecord();
       $self->{record} = { index => $1, type => "Patch", title=>$1, personalityidx => $2, dmx => $3, mode => $4, part => $5, personality => $2 };
+      next;
+    }
+    if (/^((?!\$)\S+)/) {
+      print $1,"\n";
+      $self->closerecord();
       next;
     }
     if (/^\$((?!\$)\S+)/) {
@@ -107,7 +127,7 @@ sub parse_file {
         next;
       }
     }
-    if ($self->{record} && ($self->{record}->{type} =~ /^(?:Beam|Color|Focus)Palette$/)) {
+    if ($self->{record} && ($self->{record}->{type} =~ /^(?:(?:Beam|Color|Focus)Palette|Preset)$/)) {
       if (/^\s+\$\$Param\s+(\d+)\s+(.+)$/) {
         my $chan = $1;
         if (!defined($self->{record}->{channels}->{$1})) {
@@ -214,7 +234,7 @@ sub page_start {
   my $q = shift || new CGI;
 
   $q->print("<html>\n");
-  $q->print("<head><title>Show File</title>");
+  $q->print("<head><title>".$self->{data}->{Title}."</title>");
   $q->print($self->get_styles());
   $q->print("</head>\n");
   $q->print("<body>\n");
@@ -228,6 +248,7 @@ sub generate_page {
   $anchors .= "&nbsp;<a href='\#beampalette'>Beam Palettes</a>";
   $anchors .= "&nbsp;<a href='\#colorpalette'>Color Palettes</a>";
   $anchors .= "&nbsp;<a href='\#focuspalette'>Focus Palettes</a>";
+  $anchors .= "&nbsp;<a href='\#preset'>Presets</a>";
   $anchors .= "&nbsp;<a href='\#groups'>Groups</a>";
   $anchors .= "&nbsp;<a href='\#patch'>Patch List</a>";
   $anchors .= "<br/><br/>\n";
@@ -261,7 +282,17 @@ sub generate_page {
         my $perschan = $pers->{params}->{$paramidx};
         my $size = $perschan->{size};
         my $s="";
-        if ($size==1) {
+	if ($val =~ /^(CP)(\d+)$/) {
+	   my $pal = $self->{data}->{ColorPalette}->{$2};
+	   if ($pal) { $val .= " [".$pal->{title}."]"; }
+	} elsif ($val =~ /^FP(\d+)$/) {
+	   my $pal = $self->{data}->{FocusPalette}->{$2};
+	   if ($pal) { $val .= " [".$pal->{title}."]"; }
+	} elsif ($val =~ /^BP(\d+)$/) {
+	   my $pal = $self->{data}->{BeamPalette}->{$2};
+	   if ($pal) { $val .= " [".$pal->{title}."]"; }
+	} elsif ($val =~ /^([A-Za-z]+)(\d+)$/) {
+        } elsif ($size==1) {
 	  $val = uc(unpack("H*", pack("C*", $val%256)));
 	  if ($self->{data}->{ParamNameToType}->{Red} == $paramidx) {
 	    $s = " style='font-weight: bold; color: #".$val."0000;'";
@@ -309,7 +340,17 @@ sub generate_page {
         my $perschan = $pers->{params}->{$paramidx};
         my $size = $perschan->{size};
         my $s="";
-        if ($size==1) {
+	if ($val =~ /^(CP)(\d+)$/) {
+	   my $pal = $self->{data}->{ColorPalette}->{$2};
+	   if ($pal) { $val .= " [".$pal->{title}."]"; }
+	} elsif ($val =~ /^FP(\d+)$/) {
+	   my $pal = $self->{data}->{FocusPalette}->{$2};
+	   if ($pal) { $val .= " [".$pal->{title}."]"; }
+	} elsif ($val =~ /^BP(\d+)$/) {
+	   my $pal = $self->{data}->{BeamPalette}->{$2};
+	   if ($pal) { $val .= " [".$pal->{title}."]"; }
+	} elsif ($val =~ /^([A-Za-z]+)(\d+)$/) {
+        } elsif ($size==1) {
 	  $val = uc(unpack("H*", pack("C*", $val%256)));
 	  if ($self->{data}->{ParamNameToType}->{Red} == $paramidx) {
 	    $s = " style='font-weight: bold; color: #".$val."0000;'";
@@ -357,7 +398,75 @@ sub generate_page {
         my $perschan = $pers->{params}->{$paramidx};
         my $size = $perschan->{size};
         my $s="";
-        if ($size==1) {
+	if ($val =~ /^(CP)(\d+)$/) {
+	   my $pal = $self->{data}->{ColorPalette}->{$2};
+	   if ($pal) { $val .= " [".$pal->{title}."]"; }
+	} elsif ($val =~ /^FP(\d+)$/) {
+	   my $pal = $self->{data}->{FocusPalette}->{$2};
+	   if ($pal) { $val .= " [".$pal->{title}."]"; }
+	} elsif ($val =~ /^BP(\d+)$/) {
+	   my $pal = $self->{data}->{BeamPalette}->{$2};
+	   if ($pal) { $val .= " [".$pal->{title}."]"; }
+	} elsif ($val =~ /^([A-Za-z]+)(\d+)$/) {
+        } elsif ($size==1) {
+	  $val = uc(unpack("H*", pack("C*", $val%256)));
+	  if ($self->{data}->{ParamNameToType}->{Red} == $paramidx) {
+	    $s = " style='font-weight: bold; color: #".$val."0000;'";
+	  } elsif ($self->{data}->{ParamNameToType}->{Green} == $paramidx) {
+	    $s = " style='font-weight: bold; color: #00".$val."00;'";
+	  } elsif ($self->{data}->{ParamNameToType}->{Blue} == $paramidx) {
+	    $s = " style='font-weight: bold; color: #0000".$val.";'";
+          }
+          $val = "0x".$val;
+        } elsif ($size==2) {
+	  $val = uc(unpack("H*", pack("C*", $val/256, $val%256)));
+          $val = "0x".$val;
+	} else { 
+	  $val = "";
+        }
+        $line .= "<td".$s.">".$val."</td>";
+      }
+      $chans{$chan} = $line;
+    }
+    my $output = consolidate_lines(\%chans);
+    foreach my $line (@$output) {
+      $q->print("  <tr>$line</tr>\n");
+    }
+    $q->print("</table>\n");
+  }
+  $q->print("<a name='preset'/>$anchors");
+  foreach my $key (sort { $a <=> $b } keys %{$self->{data}->{Preset}}) {
+    my $rec = $self->{data}->{Preset}->{$key};
+    $q->print("<h2>Preset ".$rec->{index}.": ".$rec->{title}."</h2>\n");
+    $q->print("<table>\n");
+    $q->print("  <tr><th>Channel(s)</th><th>Fixture</th>");
+    if ($self->{usegroups}) { $q->print("<th>Groups</th>"); }
+    $q->print(join("", map { "<th>".$self->{data}->{ParamType}->{$_}."</th>" } @{$rec->{parameters}}),"</tr>\n");
+    my %chans;
+    foreach my $chan (sort { $a <=> $b } keys %{$rec->{channels}}) {
+      my $patch = $self->{data}->{Patch}->{$chan};
+      my $persidx = $patch->{personalityidx};
+      my $pers = $self->{data}->{Personality}->{$persidx};
+      my @groups = sort { $a <=> $b } keys %{$self->{data}->{ChannelToGroups}->{$chan}};
+      my $line = "";
+      $line .= "<td>".$pers->{model}."</td>";
+      if ($self->{usegroups}) { $line .= "<td>".join(",", map { $self->{data}->{Group}->{$_}->{title}."[".$_."]" } @groups)."</td>"; }
+      foreach my $paramidx (@{$rec->{parameters}}) {
+        my $val = $rec->{channels}->{$chan}->{$paramidx};
+        my $perschan = $pers->{params}->{$paramidx};
+        my $size = $perschan->{size};
+        my $s="";
+	if ($val =~ /^(CP)(\d+)$/) {
+	   my $pal = $self->{data}->{ColorPalette}->{$2};
+	   if ($pal) { $val .= " [".$pal->{title}."]"; }
+	} elsif ($val =~ /^FP(\d+)$/) {
+	   my $pal = $self->{data}->{FocusPalette}->{$2};
+	   if ($pal) { $val .= " [".$pal->{title}."]"; }
+	} elsif ($val =~ /^BP(\d+)$/) {
+	   my $pal = $self->{data}->{BeamPalette}->{$2};
+	   if ($pal) { $val .= " [".$pal->{title}."]"; }
+	} elsif ($val =~ /^([A-Za-z]+)(\d+)$/) {
+        } elsif ($size==1) {
 	  $val = uc(unpack("H*", pack("C*", $val%256)));
 	  if ($self->{data}->{ParamNameToType}->{Red} == $paramidx) {
 	    $s = " style='font-weight: bold; color: #".$val."0000;'";
